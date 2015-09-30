cca960134c07d08cee097362488412663d1ceab4
Mike Boeser <mboeser@yahoo.com>
2015-09-29 19:43:06 -0500

    First commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,17 @@
+avengersMovie = {
+	name : "Avengers",
+	runtime : 143 + " mins",
+	releaseYear : 2012,
+	director : "Joss Whedon"
+};
+
+// out object to console like this...
+
+console.log(avengersMovie);
+
+// or like this...
+
+console.log(avengersMovie.name);
+console.log(avengersMovie.runtime);
+console.log(avengersMovie.releaseYear);
+console.log(avengersMovie.director);
